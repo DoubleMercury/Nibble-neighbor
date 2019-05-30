0e4275746f2bc5a285a2922ed4b2d52370fff205
BogacSabuncu <bsabuncu@mac.com>
2019-05-30 13:08:17 -0400

    implemented .env for config file

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
+DROP DATABASE IF EXISTS nibble_neighbor;
 -- Creates the "blogger" database --
-CREATE DATABASE blogger;
+CREATE DATABASE nibble_neighbor;
