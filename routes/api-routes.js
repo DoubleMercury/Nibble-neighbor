f5597f1958429952c7688dd645942db4fb2bba0a
BogacSabuncu <bsabuncu@mac.com>
2019-06-03 13:28:34 -0400

    added more api call functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,36 +1,47 @@
 const dbFunctions = require('../functions/dbfunctions.js');
 
 
-module.exports = function(app) {
-  // API GET Requests
+module.exports = function (app) {
+	// API GET Requests
 
-  //addUser(res, username, name, email);
-  //addRecipe(res, label, image_url, recipe_url, calories, usersID);
-  //getAllUsers(res);
-  //getAllUserRecipes
+	//addUser(res, username, name, email);
+	//addRecipe(res, label, image_url, recipe_url, calories, usersID);
+	//getAllUsers(res);
+	//getAllUserRecipes
 
-  app.post("/api/newuser", function(req, res) {
-    
-    dbFunctions.addUser(res, req.body.username, req.body.name, req.body.email);
+	app.post("/api/newuser", function (req, res) {
 
-  });
+		dbFunctions.addUser(res, req.body.username, req.body.name, req.body.email);
 
-  app.post("/api/newrecipe", function(req,res){
+	});
 
-    dbFunctions.addRecipe(res, req.body.label, req.body.image, req.body.url, req.body.calories, req.body.UserId);
-  });
+	app.post("/api/newrecipe", function (req, res) {
 
-  app.get("/api/users", function(req, res){
+		dbFunctions.addRecipe(res, req.body.label, req.body.image, req.body.url, req.body.calories, req.body.UserId);
+	});
 
-    dbFunctions.getAllUsers(res);
+	app.get("/api/users", function (req, res) {
 
-  });
+		dbFunctions.getAllUsers(res);
 
-  app.get("/api/UserRecipes/:userdId", function(req,res){
+	});
 
-    dbFunctions.getAllUserRecipes(res, req.params.userId);
+	app.get("/api/users/:userId", function (req, res) {
+		dbFunctions.getUser(res, req.params.userId)
+	});
 
-  });
+	app.get("/api/users/recipes/:userId", function (req, res) {
 
+		dbFunctions.getAllUserRecipes(res, req.params.userId);
+
+	});
+
+	app.delete("/api/users/:UserId", function (req, res) {
+		dbFunctions.deleteUser(res, req.params.UserId);
+	});
+
+	app.delete("/api/users/recipes/:RecipeId", function (req, res) {
+		dbFunctions.deleteRecipe(res, req.params.RecipeId);
+	});
 };
 
