062cf2f527fc6bf4499fa9ca75577951413611e2
Double Mercury <maafakeout@yahoo.com>
2019-06-04 13:36:53 -0400

    new html test out

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,13 +1,93 @@
-$("#submitBtn").on("click", function(){
-    console.log("clicked");
-    const protein=$("#protein_id").val();
-    console.log(protein);
-    const diet=$("#diet_id").val();
-    console.log(diet);
-    const health=$("#health_id").val();
-    console.log(health);
-    const calorie=$("#calorie_id").val();
-    console.log(calorie);
-    const exclusions = $("#exclusions_id").val().trim().replace(' ', '-');
-    console.log(exclusions)
+
+try {
+    $("#submitBtn").on("click", function(){
+        console.log("clicked");
+        const protein=$("#protein_id").val();
+        console.log(protein);
+        const diet=$("#diet_id").val();
+        console.log(diet);
+        const health=$("#health_id").val();
+        console.log(health);
+        const calorie=$("#calorie_id").val();
+        console.log(calorie);
+        const exclusions = $("#exclusions_id").val().trim().replace(' ', '-');
+        console.log(exclusions);
+    
+        
+        const queryParams = $.param({
+            "app_id" : "26abcd0f",
+            "app_key" : "448f552b9ad5ea4e801639160282843c",
+            "q": protein,
+            "diet": diet,
+            "health":health,
+            "calorie": calorie,
+            "from":0,
+            "to":6
+        });
+    
+        const queryUrl="https://api.edamam.com/search?" + queryParams;
+        //q=" + protein + "&app_id=26abcd0f&app_key=448f552b9ad5ea4e801639160282843c&from=0&to=5&calories=" + calorie + "&diet=" + diet;
+    
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log("response: ",response);
+            for(i=0;i<response.hits.length;i++){
+            const recipe = response.hits[i].recipe;
+            const label = recipe.label;
+            const image = recipe.image;
+            const url = recipe.uri;
+            const calories = recipe.calories;
+            recipeRender(image,label,calories,url);
+            console.log("recipes: ",recipe);
+            console.log("label: ", label);
+            console.log("label: ", image);
+            console.log("label: ", url);
+            console.log("label: ", calories);
+
+
+            }
+            $("#resultsModal").addClass("is-active");
+        });
+    
     });
+}
+
+catch(err) {
+    $("#resultsModal").append("Sorry Neighbor, no recipes matching your request was found...")
+}
+
+
+
+
+function recipeRender(image,label,calories,url) {
+        $("#resultsModal").append(`
+        <div class="card">
+        <div class="card-image">
+          <figure class="image is-128x128">
+            <img src="${image}" alt="image">
+          </figure>
+        </div>
+        <div class="card-content">
+                <div class="media-content">
+              <p class="title is-4">${label}</p>
+              <p class="subtitle is-6">${calories}</p>
+              <p>${url}</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            Recipe contents and directions.
+            <a class="button">Save Recipe</a>
+            <br>
+          </div>
+        </div>
+      </div>
+        `);
+}
+
+
+
+
+
