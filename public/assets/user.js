37fccb644170ed86a41da722226ff8e342c9a7eb
darylbre <roderickbreland@gmail.com>
2019-06-05 10:25:48 -0400

    update user page with functions

---
+++ public/assets/user.js
@@ -0,0 +1,68 @@
+const userID = localStorage.getItem("userId");
+
+
+function userPageRender() {
+    $.get(`/api/users/${userID}`).then(function(user){
+        // const heroBody = $(`<div class = hero-body>`).appendTo($("#userPageHeader"));
+        // heroBody.append(`<h1 class="heading userInfo title is-size-3 is-block" >${user.username}</1>`);
+
+        // heroBody.append(`<h2 class="heading userInfo subtitle is-size-4 is-block" >${user.name}</h2>`);
+
+        // heroBody.append(`<h2 class="heading userInfo subtitle is-size-4 is-inline-block" >${user.email}</h2>`);
+
+        $("#userPageHeader").append(`<div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+          ${user.username}
+          </h1>
+          <h2 class="subtitle">
+          ${user.name}
+          </h2>
+          <h2 class="subtitle">
+          ${user.email}
+          </h2>
+        </div>
+      </div>`)
+
+    })
+
+    
+
+}
+
+function makeRecipeCard(recipe){
+$("#userMainContent").append(`
+<div class="column is-one-third is-inline-block">
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src=${recipe.image} alt="Food Image">
+    </figure>
+    </div>
+    <div class="media-content">
+        <p class="title is-4">${recipe.label}</p>
+    </div>
+    <div class="card-content">
+        <div class="content">
+         ${recipe.calories} Calories
+         </div>
+    </div>
+    <footer class="card-footer">
+    <a href="${recipe.url}" class="card-footer-item">View</a>
+    <button class="card-footer-item delete-button">Delete</Delete>
+  </footer>
+</div>
+</div>
+  `)
+}
+
+function renderRecipes() {
+    $.get(`/api/users/recipes/${userID}`).then(function(recipes){
+        recipes.forEach(recipe => {
+            makeRecipeCard(recipe);
+        });
+    });
+}
+
+userPageRender();
+renderRecipes();
